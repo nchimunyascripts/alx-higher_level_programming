9d171649a84fb188627969dbc10ffe935cbcf717
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-01-24 03:55:43 +0200

    refactor

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
-    return ("BestSchool, " * getattr(magic_string, "n", 0))[:-2]
--- 0x15-javascript-web_jquery/1-script.js
+++
@@ -1,3 +0,0 @@
-// Uses the jQuery API to change the color of the header tag to red
-
-$('header').css('color', '#FF0000');
