111eaa266f293c124dfc10a87b8ac25ed98ed308
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-03-15 04:40:10 +0200

    some

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- convert all of the following to UTF8
 -- mysqldump -uroot -p -\-default-character-set=utf8 -\-no-create-db hbtn_0c_0 > hbtn_0c_0.sql
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
