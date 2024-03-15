080b317da20ce0b6ef6d5a2a638bd4aff1381b78
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-03-15 04:38:08 +0200

    complete

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- lists all shows contained in hbtn_0d_tvshows
-SELECT a.title, b.genre_id
-FROM tv_shows a, tv_show_genres b
-WHERE b.show_id = a.id
-ORDER BY a.title ASC, b.genre_id ASC;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
